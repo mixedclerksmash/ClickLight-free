e95344bcaac26d76ece7e5c2b2a1aaf827816b1a
mixedclerksmash <stephainepauls@gmail.com>
2026-06-09 15:44:33 +0200

    Add file

---
+++ Casks/clicklight.rb
@@ -0,0 +1,20 @@
+cask "clicklight" do
+  auto_updates true
+  version "0.14.1"
+  sha256 "b0ec1618f3056a81012313def8008bc0dd616df8aa7c03cbd458351151f43328"
+
+  url "https://github.com/aurorascharff/ClickLight/releases/download/v#{version}/ClickLight.zip"
+  name "ClickLight"
+  desc "Highlight clicks anywhere on your Mac for live demos"
+  homepage "https://github.com/aurorascharff/ClickLight"
+
+  app "ClickLight.app"
+
+  postflight do
+    system "xattr", "-cr", "#{appdir}/ClickLight.app"
+  end
+
+  zap trash: [
+    "~/Library/Preferences/com.aurorascharff.ClickLight.plist",
+  ]
+end
